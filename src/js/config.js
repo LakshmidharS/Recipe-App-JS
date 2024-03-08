019c31c4e8a5c2ec869ac6803864d8e831ab3aba
LakshmidharS <urstrulylakshmidhar@gmail.com>
2024-03-08 16:57:46 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '81461d52-cf57-4bdf-a6bc-165d096c9476';
+export const MODAL_CLOSE_SEC = 2.5;
